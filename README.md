20bcf52b9b69015d80c946bc96d3679ae80d6922
Eric Harper <harper28@pdx.edu>
2020-01-11 22:13:21 -0800

    Finished with Homework 1

---
+++ README.md
@@ -0,0 +1,28 @@
+#Modexp
+
+This is a command-line calculator that calculates an exponential modulo some number
+
+#Usage
+
+```bash
+cargo run 2 20 17
+```
+
+#What I did
+
+I began by writing a harc-coded unit test, to ensure my test behaved properly.
+I then wrote ```Rust modexp()``` to perform the desired calculation. I used to pseudocode
+provided to structure my algorithm for the function. I added error handling
+for invalid input values. The main function was the last to be accounted for.
+There, I added support to parse the command line arguments into an array
+and used the values as arguments to ```Rust modexp()```.
+
+#How it went
+
+Everything came together well. I made sure to code iteratively, by planning,
+implementing, testing, and evaluating. I have to thank Rust's clear error
+messages for helping me resolve my syntax issues.
+
+#How I tested
+
+I tested a variety of values using ```Rust assert_eq!()```, hoping to cover any edge cases. 
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#[test]
+fn test_modexp() {
+    assert_eq!(modexp(2, 20, 17), 16);
+    assert_eq!(modexp(0, 0, 0), 0);
+    assert_eq!(modexp(2, 4, 1), 0);
+    assert_eq!(modexp(2, 0, 1), 0);
+}
+
+fn modexp(x: u64, y: u64, m: u64) -> u64 {
+    if m == 1 {
+        return 0;
+    }
+    if x == 0 {
+        return 0;
+    }
+    if y == 0 {
+        return 1;
+    }
+
+    let mut z: u64 = modexp(x, y / 2, m);
+    z = (z * z) % m;
+
+    if (y % 2) == 1 {
+        z = (z * x) % m;
+    }
+
+    z
+}
+
+fn error() -> ! {
+    eprintln!("modexp: usage: modexp <x>, <y>, <m>");
+    std::process::exit(1);
+}
+
+fn main() {
+    // this is used for skipping prog name in cmd-line args
+    let mut argv: [u64; 3] = [0; 3];
+    let mut i = 0;
+    for arg in std::env::args().skip(1) {
+        argv[i] = arg.parse().unwrap_or_else(|_| error());
+        i += 1;
+    }
+
+    if argv[2] == 0 {
+        error();
+    }
+
+    let res = modexp(argv[0], argv[1], argv[2]);
+
+    println!(
+        "The result of ({}**{}) mod {} is: {}",
+        argv[0], argv[1], argv[2], res
+    );
+}
